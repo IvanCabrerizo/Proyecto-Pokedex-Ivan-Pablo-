e08c6e362301feb57fb8063a724bbe01feea8eec
Pablo Muñoz <pabloway17@gmail.com>
2023-02-21 11:34:13 +0100

    Third merge test

--- app/src/main/java/com/example/pokedex_project/MainActivity.kt
+++ app/src/main/java/com/example/pokedex_project/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Este es el merge a la rama master")
+    Text(text = "Este es el merge a la rama master" + name)
 }
 
 @Preview(showBackground = true)
