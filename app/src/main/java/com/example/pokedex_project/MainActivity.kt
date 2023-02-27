7a1c5bbe8c3f2765a2375da83bbee01abaea78b2
Pablo Muñoz <pabloway17@gmail.com>
2023-02-27 12:48:21 +0100

    Comienzo de trabajo en el l login

--- app/src/main/java/com/example/pokedex_project/MainActivity.kt
+++ app/src/main/java/com/example/pokedex_project/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Hello my name is Pablo")
+                    Greeting()
                 }
             }
         }
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Greeting() {
     Text(text = "Este es el merge a la rama master")
 }
 
@@ -38,6 +38,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     PokedexProjectTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
