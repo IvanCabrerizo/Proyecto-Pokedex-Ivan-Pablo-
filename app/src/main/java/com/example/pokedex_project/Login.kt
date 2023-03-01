c4884e42dc6d7124bb6ebeb92b65f23879f38cbe
Pablo Muñoz <pabloway17@gmail.com>
2023-03-01 11:03:04 +0100

    Login without final colors

--- app/src/main/java/com/example/pokedex_project/Login.kt
+++ app/src/main/java/com/example/pokedex_project/Login.kt
@@ -16,8 +16,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.pokedex_project.R
 import com.example.pokedex_project.ui.theme.PokedexProjectTheme
@@ -48,27 +50,24 @@ class Login : ComponentActivity() {
 
 @Composable
 fun LoginScreen() {
-    ConstraintLayout(Modifier.fillMaxSize()) {
-        val (header, body) = createRefs()
-        /*
-        *
-        *Autor: Pablo Muñoz
-        *Cabezera de la aplicacion ya desarrollada
-        *
-        * */
-        Column(Modifier.fillMaxSize()) {
-            Box(
-                Modifier
-                    .fillMaxHeight(0.2f)
-                    .fillMaxWidth()) {
-                    Column(Modifier.fillMaxSize()) {
-                        Spacer(modifier = Modifier
-                            .fillMaxWidth()
-                            .height(10.dp))
-                        Header()
-                    }
-
-                }
+    /*
+     *
+     *Autor: Pablo Muñoz
+     *Cabezera de la aplicacion ya desarrollada
+     *
+     * */
+    Column(Modifier.fillMaxSize()) {
+        Box(
+            Modifier
+                .fillMaxHeight(0.2f)
+                .fillMaxWidth()) {
+            Column(Modifier.fillMaxSize()) {
+                Spacer(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(30.dp))
+                Header()
+            }
+        }
             Box(
                 Modifier
                     .fillMaxSize()){
@@ -76,7 +75,6 @@ fun LoginScreen() {
                 }
         }
 
-    }
 }
 /*
 *
@@ -101,23 +99,23 @@ fun Header(){
                 }
         )
         Image(
-            painter = painterResource(id = R.drawable.charmander),
-            contentDescription ="Foto con el nombre pokedex para el login",
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription ="Foto con el nombre pokemon para el login",
             Modifier
-                .height(100.dp)
+                .height(60.dp)
                 .constrainAs(foto2) {
                     top.linkTo(parent.top)
                     bottom.linkTo(texto.top)
                     start.linkTo(foto1.end)
                     end.linkTo(parent.end)
                 })
-        Text(text = "End of degree project",
+        Text(text = "End of degree \n project",
             Modifier.constrainAs(texto) {
                 top.linkTo(foto2.bottom)
                 bottom.linkTo(parent.bottom)
                 start.linkTo(foto2.start)
                 end.linkTo(foto2.end)
-            }
+            }, fontSize = 26.sp, textAlign = TextAlign.Center
         )
     }
 }
@@ -137,7 +135,7 @@ fun Body(){
             .fillMaxWidth()
             .fillMaxSize(0.07f))
 
-        Text(text = "Username", Modifier.padding(60.dp, 16.dp))
+        Text(text = "Username", Modifier.padding(60.dp, 12.dp), fontSize = 20.sp)
 
         TextField(
             value = nombre,
@@ -145,7 +143,7 @@ fun Body(){
             Modifier.padding(40.dp, 16.dp)
         )
 
-        Text(text = "Password",  Modifier.padding(60.dp, 16.dp))
+        Text(text = "Password",  Modifier.padding(60.dp, 12.dp), fontSize = 20.sp)
 
         TextField(
             value = constrasena,
@@ -155,7 +153,7 @@ fun Body(){
         Spacer(
             Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.5f))
+                .fillMaxHeight(0.4f))
         Row(Modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceEvenly) {
             Button(onClick = { /*TODO*/ }) {
                 Text(text = "Login")
