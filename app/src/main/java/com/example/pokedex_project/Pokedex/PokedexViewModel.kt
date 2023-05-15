0f8cf55e73a2b21cde4504baf06e4f5271b0fa0f
IvanLaptop <i.cabrerizo@cesjuanpablosegundocadiz.es>
2023-05-15 16:18:35 +0200

    No se

--- app/src/main/java/com/example/pokedex_project/Pokedex/PokedexViewModel.kt
+++ app/src/main/java/com/example/pokedex_project/Pokedex/PokedexViewModel.kt
@@ -1,39 +1,34 @@
 package com.example.pokedex_project.Pokedex
 
 import androidx.compose.material.ScaffoldState
-import androidx.compose.material.icons.Icons
-import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.pokedex_project.model.Pokemon
 import com.example.pokedex_project.repository.PokemonRepository
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class PokedexViewModel:ViewModel() {
+class PokedexViewModel : ViewModel() {
 
     private val _scaffoldState = MutableLiveData<ScaffoldState>()
     val scaffoldState = _scaffoldState
 
-    fun onPokedexChange(scaffoldState: ScaffoldState){
+    fun onPokedexChange(scaffoldState: ScaffoldState) {
         _scaffoldState.value = scaffoldState
     }
-
-
 }
 
-fun onMenuPressed(coroutineScope: CoroutineScope, scaffoldState: ScaffoldState){
+fun onMenuPressed(coroutineScope: CoroutineScope, scaffoldState: ScaffoldState) {
     coroutineScope.launch { scaffoldState.drawerState.open() }
 }
-fun onCloseDrawer(coroutineScope: CoroutineScope, scaffoldState: ScaffoldState){
+
+fun onCloseDrawer(coroutineScope: CoroutineScope, scaffoldState: ScaffoldState) {
     coroutineScope.launch { scaffoldState.drawerState.close() }
 }
+
 suspend fun getPokemons(): List<Pokemon> {
-    /*
-    *
-    * Ivan aquí va la lista de pokemons
-    *
-    *
-    * */
     val pokemonRepository = PokemonRepository()
     return withContext(Dispatchers.Main) {
         pokemonRepository.getAllPokemon()
